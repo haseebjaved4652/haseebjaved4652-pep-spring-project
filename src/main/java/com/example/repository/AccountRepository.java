3cf030ce7b3bd91929396459d5d3d74b922b820a
haseeb javed <haseebjaved4652@gmail.com>
2025-05-17 03:10:44 +0000

    Commit Project

--- src/main/java/com/example/repository/AccountRepository.java
+++ src/main/java/com/example/repository/AccountRepository.java
@@ -1,22 +1,4 @@
 package com.example.repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
-
-import com.example.entity.Account;
-
-public interface AccountRepository extends JpaRepository<Account, Integer>{
-
-    //@Modifying
-    //@Transactional
-    /* @Query("INSERT INTO Account(username,password) values (?,?)") 
-    void newUserAccountRegistration(@Param("username") String username, @Param("password") String password);
-    @Query("SELECT username FROM Account WHERE username=?")
-    public String getUsernameString(Account account); */
-    @Query("FROM Account WHERE username=:username")
-    public Account searchAccountByUsername(@Param("username") String username);
-    public Account searchAccountByUsernameAndPassword(String username, String passwrod);
-    public Account searchAccountByAccountId(Integer accountId);
-
+public interface AccountRepository {
 }
--- src/main/java/com/example/repository/MessageRepository.java
+++ src/main/java/com/example/repository/MessageRepository.java
@@ -1,19 +1,4 @@
 package com.example.repository;
 
-import java.util.List;
-import java.util.Optional;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-
-import com.example.entity.Message;
-
-
-public interface MessageRepository extends JpaRepository<Message, Integer> {
-
-
-    @Query("SELECT m FROM Message m WHERE m.postedBy = ?1")
-    public List<Message> fetchByPostedBy(Integer postedBy);
-
-    //boolean existsById(Integer postedBy);
+public interface MessageRepository {
 }
